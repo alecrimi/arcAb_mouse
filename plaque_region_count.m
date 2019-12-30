e41dbbeb8f4e13d831cb2c4fe8b4562a95bee340
Alessandro Crimi <alecrimi@alice.it>
2019-12-30 01:49:49 +0100

    Update plaque_region_count.m

--- plaque_region_count.m
+++ plaque_region_count.m
@@ -1,37 +1,33 @@
+function plaque_region_count(fname,fname_atlas_lables)
+
 %Parameters
-n_substacks = 225;%500;
-fname  = 'old_left_scaled.tif';%'left_c.tif';
+%n_substacks = 500;
+%fname  = 'old_left_scaled.tif';%'left_c.tif';
 info = imfinfo(fname);
-
 num_images =  numel(info);
-
-%numFramesStr = regexp(info.ImageDescription, 'images=(\d*)', 'tokens');
-%num_images =  str2double(numFramesStr{1}{1});
-fname_atlas = 'revert\result.tif';%'result_atlas.tif ';
+%fname_atlas_lables = 'revert\result.tif';%'result_atlas.tif ';
 info2 = imfinfo(fname_atlas);
 grouped_region = xlsread('regions_columns.xls');
-[ sub, roi] = size(grouped_region);
+[ sub, roi ] = size(grouped_region);
 grouped_region(isnan(grouped_region))=0;
-res = zeros(floor(num_images/n_substacks),roi);
-   
-%load  partially 
+%res = zeros(floor(num_images/n_substacks),roi);
+res = zeros(1,roi);
+se = offsetstrel('ball',4,4); %Erosion element in case of need to separate the ROIs
+ 
+%  Load data
 temp = imread(fname);
 [r,c] = size(temp);
-%c = c -4 ;
-A = zeros(r,c,n_substacks,'uint16');
-reg_atlas = zeros(r,c,n_substacks,'uint16');
+volume = zeros(r,c,n_substacks,'uint16');
+reg_atlas_lables = zeros(r,c,n_substacks,'uint16');
 
 count = 1;
 for k = 1     :    num_images
-        A(:,:,count ) = (imread(fname, k)); %imadjust
-        k
+        volume(:,:,count ) = (imread(fname, k)); %imadjust
         count = count +1;
 end
-
 count = 1;
 for k = 1     :    num_images
-        reg_atlas(:,:,count ) = (imread(fname_atlas, k)); %imadjust
-        k
+        reg_atlas_lables(:,:,count ) = (imread(fname_atlas_lables, k)); %imadjust
         count = count +1;
 end
 
@@ -43,8 +39,6 @@ end
 % the INFO for this file, each image consists of 1 strip.
 %fseek(fp, info.StripOffsets, 'bof');
 %fseek(fp2, info2.StripOffsets, 'bof');
-%count = 1;
-se = offsetstrel('ball',4,4);
 
 %for k = 1:  n_substacks:  num_images - n_substacks
 %k
@@ -57,9 +51,9 @@ se = offsetstrel('ball',4,4);
     end
     %}
     %reg_atlas = reg_atlas - 32768; %Adjust from imagej values
-    T = adaptthresh(A);       
-    bw=imbinarize(A,max(max(max(T))));
-   % saveastiff(uint8(bw), [ 'seg2.tif']);
+    T = adaptthresh(volume);       
+    bw=imbinarize(volume,max(max(max(T))));
+    saveastiff(uint8(bw), [ 'segmentation.tif']);
     
     %Create mask according to region
     for ll = 1  :     roi
@@ -67,18 +61,7 @@ se = offsetstrel('ball',4,4);
         mask = zeros(r,c,n_substacks,'uint16');
         this_roi = grouped_region(:,ll);
         this_roi(this_roi==0) = [];
-        %{
-        for zz = 1 : n_substacks
-        for rr = 1 : r
-            for cc = 1 : c
-                
-                if ( any(reg_atlas(rr,cc,zz) == this_roi )) 
-                    mask(rr,cc,zz) = 1;
-                end
-            end
-        end
-        end
-        %}
+
         for zz = 1 : length(this_roi)
             %zz
             indices = find( reg_atlas == this_roi(zz) ) ;
@@ -94,12 +77,11 @@ se = offsetstrel('ball',4,4);
     %        mask(:,:,tt) = imerode( mask(:,:,tt),se);
     %    end
         
-        layer = mask.* uint16(bw);%A ;
+        layer = mask.* uint16(bw); 
         %Segment according to global threshold automatically
        
         res(1,ll) = max(max(max(bwlabeln(layer))));
     end
-  %      count = count + 1 ;
 %end
 %fclose(fp); 
 %fclose(fp2); 
